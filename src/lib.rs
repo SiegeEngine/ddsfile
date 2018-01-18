69c2cb6387be89c1ce907b0db5c4634cd49a3885
Mike Dilger <mike@optcomp.nz>
2018-01-19 12:31:39 +1300

    Dds file

--- src/lib.rs
+++ src/lib.rs
@@ -42,3 +42,50 @@ pub use header::{Header, HeaderFlags, Caps, Caps2};
 
 mod header10;
 pub use header10::{Header10, DxgiFormat, D3D10ResourceDimension};
+
+use std::io::{Read, Write};
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+
+pub struct Dds {
+    // magic is implicit
+    pub header: Header,
+    pub header10: Option<Header10>,
+    pub data: Vec<u8>,
+}
+
+impl Dds {
+    const MAGIC: u32 = 0x20534444; // b"DDS " in little endian
+
+    pub fn read<R: Read>(r: &mut R) -> Result<Dds> {
+        let magic = r.read_u32::<LittleEndian>()?;
+        if magic != Self::MAGIC {
+            return Err(ErrorKind::BadMagicNumber.into());
+        }
+
+        let header = Header::read(r)?;
+
+        let header10 = if header.spf.fourcc == FourCC(<FourCC>::DX10) {
+            Some(Header10::read(r)?)
+        } else {
+            None
+        };
+
+        let mut data: Vec<u8> = Vec::new();
+        r.read_to_end(&mut data)?;
+        Ok(Dds {
+            header: header,
+            header10: header10,
+            data: data
+        })
+    }
+
+    pub fn write<W: Write>(&self, w: &mut W) -> Result<()> {
+        w.write_u32::<LittleEndian>(Self::MAGIC)?;
+        self.header.write(w)?;
+        if let Some(ref header10) = self.header10 {
+            header10.write(w)?;
+        }
+        w.write_all(&self.data)?;
+        Ok(())
+    }
+}
